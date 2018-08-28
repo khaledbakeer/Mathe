369fb4158787fccebcba640a4b50d528e37799dd
kbakeer <bakeer@khaled.de>
2018-08-28 19:41:31 +0200

    Frei Rechnen Methods Vorbereiten

---
+++ src/includes/Anweisungen/FreiRechnen/Analysieren.java
@@ -0,0 +1,112 @@
+package includes.Anweisungen.FreiRechnen;
+
+/**
+ * Analysieren Klasse
+ */
+public class Analysieren extends FreiRechnen {
+
+    public int pos = -1;
+    public int ch;
+    public String str = super.getTextOhneSpaces();
+
+    /**
+     * Überprüft den nächsten Char.
+     */
+    void nextChar() {
+        if (++pos < str.length()) {
+            ch = str.charAt(pos);
+        } else {
+            ch = -1;
+        }
+    }
+
+    /**
+     * @param str
+     * @return
+     */
+    public double analysieren(String str) {
+        this.str = str;
+        nextChar();
+        return ausdruckAnalysieren();
+    }
+
+    /**
+     * @param gerade
+     * @return
+     */
+    boolean charIst(int gerade) {
+        while (ch == ' ') nextChar();
+        if (ch == gerade) {
+            nextChar();
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * @return
+     */
+    double ausdruckAnalysieren() {
+        double x = begriffAnalysieren();
+        for (; ; ) {
+            if (charIst('+')) x += begriffAnalysieren(); // +
+            else if (charIst('-')) x -= begriffAnalysieren(); // -
+            else return x;
+        }
+    }
+
+    /**
+     * @return
+     */
+    double begriffAnalysieren() {
+        double x = factorAnalysieren();
+        for (; ; ) {
+            if (charIst('*')) x *= factorAnalysieren(); // *
+            else if (charIst('/')) x /= factorAnalysieren(); // /
+            else return x;
+        }
+    }
+
+    /**
+     * @return
+     */
+    double factorAnalysieren() {
+        if (charIst('+')) return factorAnalysieren(); // unary plus
+        if (charIst('-')) return -factorAnalysieren(); // unary minus
+
+        double x;
+        int startPos = this.pos;
+        if (charIst('(')) { // parentheses
+            x = ausdruckAnalysieren();
+            charIst(')');
+        } else if ((ch >= '0' && ch <= '9') || ch == '.') { // Nummern
+            while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+            x = Double.parseDouble(str.substring(startPos, this.pos));
+        } else if (ch >= 'a' && ch <= 'z') { // functions
+            while (ch >= 'a' && ch <= 'z') nextChar();
+            String func = str.substring(startPos, this.pos);
+            x = factorAnalysieren();
+            if (func.equals("sqrt")) x = Math.sqrt(x);
+            else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+            else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+            else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+            else throw new RuntimeException("Unknown function: " + func);
+        } else {
+            throw new RuntimeException("Unexpected: " + (char) ch);
+        }
+
+        if (charIst('^')) x = Math.pow(x, factorAnalysieren()); // exponentiation
+
+        return x;
+    }
+
+
+    /**
+     * @return
+     */
+    @Override
+    public String Result() {
+        return null;
+    }
+}
---
+++ src/includes/Anweisungen/FreiRechnen/FreiRechnen.java
@@ -0,0 +1,20 @@
+package includes.Anweisungen.FreiRechnen;
+
+import includes.Anweisungen.Anweisung;
+
+/**
+ * Frei Rechnen Klasse
+ */
+public abstract class FreiRechnen extends Anweisung {
+
+    /**
+     * @return
+     */
+    public abstract String Result();
+
+    /**
+     * Konstruktor
+     */
+    public FreiRechnen() { }
+
+}
