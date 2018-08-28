ede6113dd4010690330105f2054329405d53442c
kbakeer <bakeer@khaled.de>
2018-08-28 20:01:08 +0200

    add documentation 0.1

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"Configurations"},{"l":"includes"},{"l":"includes.Anweisungen"},{"l":"includes.Anweisungen.FreiRechnen"},{"l":"includes.Anweisungen.Rechnen"},{"l":"Views"}]
